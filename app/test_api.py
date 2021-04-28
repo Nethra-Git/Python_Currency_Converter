7098cc8baaf7b44d6026ea7b35ac985d4c8b6145
Nethra-addressya <nethra@addressya.com>
2021-04-28 17:32:16 +0200

    Added one more test for testing http response code

--- app/test_api.py
+++ app/test_api.py
@@ -13,7 +13,12 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(currency_converter({'amount': 100, 'src_currency': 'SEK', 'dest_currency': 'USD'}), {"error": "Invalid input Currency, only EUR as base currency is supported for now"}) 
         
     def test_same_src_dest_currency(self): 
-        self.assertEqual(currency_converter({'amount': 100, 'src_currency': 'EUR', 'dest_currency': 'EUR'}), {"error": "Invalid operation, Change Destination currency to SEK"}) 
+        self.assertEqual(currency_converter({'amount': 100, 'src_currency': 'EUR', 'dest_currency': 'EUR'}), {"error": "Invalid operation, Change Destination currency to SEK"})
+        
+    def test_url_response_code_equals_200(self):
+        url = "http://data.fixer.io/api/latest?access_key=e893e2e8622b73546983d793c7af3643&symbols=SEK"
+        resp = requests.get(url)
+        assert resp.status_code == 200    
        
 if __name__ == '__main__':
     unittest.main()
